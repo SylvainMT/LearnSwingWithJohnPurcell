e322c2bc926b013924e7279905acf6c0a1252b69
Sylvain Tousignant <sylvain_tousignant@outlook.com>
2023-02-21 20:31:59 -0500

    Lesson 4 Completed

---
+++ src/main/java/ca/sylvain/jpurcellswingtutorial/App.java
@@ -0,0 +1,9 @@
+package ca.sylvain.jpurcellswingtutorial;
+
+import javax.swing.*;
+
+class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MainFrame::new);
+    }
+}
---
+++ src/main/java/ca/sylvain/jpurcellswingtutorial/MainFrame.java
@@ -0,0 +1,29 @@
+package ca.sylvain.jpurcellswingtutorial;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+class MainFrame extends JFrame {
+    private JTextArea textArea;
+    private JButton btn;
+    MainFrame () {
+        super("Hello World");
+
+        setLayout(new BorderLayout());
+
+        textArea = new JTextArea();
+        btn = new JButton("Click Me!");
+
+        btn.addActionListener((ActionEvent ae) -> {
+            textArea.append("Hello\n");
+        });
+
+        add(textArea, BorderLayout.CENTER);
+        add(btn, BorderLayout.SOUTH);
+
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
